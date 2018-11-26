02b2f7397a2d2501ffe77c00a0dafc508e9e3071
Ricardo Montes <tognolamontes@yahoo.com.br>
2018-11-26 18:36:00 -0200

    fix the repited routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:new, :create]
-  resources :venue
-  resources :booking, only: [:index, :new, :create, :show]
+  resources :users, only: [] do
+    resources :venue
+  end
+
+  resources :venue, only: [] do
+    resources :booking, only: [:index, :new, :create, :show]
+  end
 end
