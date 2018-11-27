d7e2ef4548bf5b45d90e847f5768e3f7e3e22f5d
Ricardo Montes <tognolamontes@yahoo.com.br>
2018-11-27 15:22:17 -0200

    Done: Venues controller methods and respective Views.

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,2 +1,48 @@
 class VenuesController < ApplicationController
+  before_action :set_user, only: [:show, :new, :create, :edit, :update, :destroy]
+  def index
+    @venues = Venue.all
+  end
+
+  def show
+    @venue = Venue.find(params[:id])
+  end
+
+  def new
+    @venue = Venue.new
+  end
+
+  def create
+    @venue = Venue.new(venue_params)
+    @venue.user = @user
+    @venue.save
+    redirect_to user_venue_path(@user, @venue)
+  end
+
+  def edit
+    @venue = Venue.find(params[:id])
+  end
+
+  def update
+    @venue = Venue.find(params[:id])
+    @venue.update(venue_params)
+    redirect_to user_venue_path(@user, @venue)
+  end
+
+  def destroy
+    @venue = Venue.find(params[:id])
+    @venue.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def venue_params
+    params.require(:venue).permit(:name, :address, :category, :price)
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [] do
-    resources :venue
+    resources :venues
   end
 
-  resources :venue, only: [] do
-    resources :booking, only: [:index, :new, :create, :show]
+  resources :venues, only: [] do
+    resources :bookings, only: [:index, :new, :create, :show]
   end
 end
