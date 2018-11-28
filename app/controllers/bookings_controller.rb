4da959f8e6efca7f1d64a9c7143cb00e42d80021
Rodrigoassuf <rodrigoassuf@hotmail.com>
2018-11-28 12:19:38 -0200

    Bookings controller created.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,51 @@
 class BookingsController < ApplicationController
+  before_action :set_venue
+  # before_action :set_user, only: [:index, :show, :new, :create]
+
+  def index
+    @bookings = @venue.bookings.where(user: current_user)
+  end
+
+  def show
+    @booking = @venue.bookings.find(params[:id])
+  end
+
+  def new
+    @booking = @venue.bookings.new
+  end
+
+  def create
+    @booking = @venue.bookings.new(booking_params)
+    @booking.user = current_user
+    @booking.price = (@booking.final_date - @booking.initial_date) * @venue.price
+    if @booking.save
+      redirect_to venue_booking_path(@venue, @booking)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @booking = @venue.bookings.find(params[:user_id])
+  end
+
+  def update
+    @booking = @venue.bookings.find(params[:user_id])
+    @booking.update(booking_params)
+    redirect_to venue_booking_path(@venue, @booking)
+  end
+
+  private
+
+  def set_venue
+    @venue = Venue.find(params[:venue_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:initial_date, :final_date)
+  end
+
+  # def set_user
+  #   @user = User.find(params[:user_id])
+  # end
 end
