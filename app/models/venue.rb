ff7b86517850a5e25a68ef85af057f79cb20372a
Rodrigoassuf <rodrigoassuf@hotmail.com>
2018-11-29 14:39:01 -0200

    Seeds created.

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -3,4 +3,8 @@ class Venue < ApplicationRecord
   has_many :bookings
   # geocoded_by :address
   # after_validation :geocode, if: :will_save_change_to_address?
+
+  def self.categories
+    VENUE_TYPES
+  end
 end
