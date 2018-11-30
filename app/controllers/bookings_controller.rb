55e4f434f1bedacacd40c9d22136d5c5b67f2ba1
Milene Cardoso <m.cardoso@hotmail.fr>
2018-11-30 15:43:46 -0200

    fixed bugs

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -30,11 +30,11 @@ class BookingsController < ApplicationController
   end
 
   def edit
-    @booking = @venue.bookings.find(params[:user_id])
+    @booking = @venue.bookings.find(params[:id])
   end
 
   def update
-    @booking = @venue.bookings.find(params[:user_id])
+    @booking = @venue.bookings.find(params[:id])
     @booking.update(booking_params)
     redirect_to venue_booking_path(@venue, @booking)
   end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -8,8 +8,6 @@ class Venue < ApplicationRecord
   validates :category, inclusion: { in: VENUE_TYPE }
   validates :name, presence: true
   validates :price, presence: true
-  validates :latitude, numericality: { greater_than_or_equal_to: -90, less_than_or_equal_to: 90 }
-  validates :longitude, numericality: { greater_than_or_equal_to: -180, less_than_or_equal_to: 180 }
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 
